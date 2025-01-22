288bc054dbd1210d16279a95580a9df5de488fca
Xostron <aat9044107480@gmail.com>
2025-01-22 23:54:47 +0300

    fix recursion intersection

--- back/src/control/save/fn/index.js
+++ back/src/control/save/fn/index.js
@@ -1,5 +1,5 @@
-const { data: store, setPos, setTuneTime, setTick } = require('@store')
-const { stateV } = require('@tool/command/valve')
+const { data: store, setPos, setTuneTime, setTick } = require("@store")
+const { stateV } = require("@tool/command/valve")
 
 // Прогресс открытия/закрытия клапана (сохранение в retain)
 function positionVlv(obj) {
@@ -10,7 +10,12 @@ function positionVlv(obj) {
 		const buildingId = output[idOn].buildingId
 		const section = data.section.find((s) => vlv.sectionId.includes(s._id))
 		const total = retain?.[section?.buildingId]?.valve?.[vlv._id]
-		const state = stateV(vlv._id, value, section?.buildingId, vlv.sectionId[0])
+		const state = stateV(
+			vlv._id,
+			value,
+			section?.buildingId,
+			vlv.sectionId[0]
+		)
 		// Текущее положение клапана из retain
 		let vlvPos
 		for (const build in retain) {
@@ -19,7 +24,7 @@ function positionVlv(obj) {
 		}
 
 		// открывается
-		if (state === 'iopn') {
+		if (state === "iopn") {
 			const cur = vlvPos?.[buildingId][vlv._id] + store._cycle_ms_
 			// ограничение диапазона хода
 			const value = cur > total ? total : cur
@@ -28,7 +33,7 @@ function positionVlv(obj) {
 		}
 
 		// закрывается
-		if (state === 'icls') {
+		if (state === "icls") {
 			const cur = vlvPos?.[buildingId]?.[vlv?._id] - store._cycle_ms_
 			// ограничение диапазона хода
 			const value = cur < 0 ? 0 : cur
@@ -36,9 +41,11 @@ function positionVlv(obj) {
 		}
 
 		// Открыт
-		if (state === 'opn') setPos({ _id: vlv._id, _build: buildingId, value: total })
+		if (state === "opn")
+			setPos({ _id: vlv._id, _build: buildingId, value: total })
 		// Закрыт
-		if (state === 'cls') setPos({ _id: vlv._id, _build: buildingId, value: 0 })
+		if (state === "cls")
+			setPos({ _id: vlv._id, _build: buildingId, value: 0 })
 	})
 }
 
@@ -68,7 +75,7 @@ function cbTune(obj, data) {
 	for (const build in obj) {
 		result[build] = {
 			...result[build],
-			valve: { ...result?.[build]?.['valve'], ...obj[build] },
+			valve: { ...result?.[build]?.["valve"], ...obj[build] },
 		}
 	}
 	setTuneTime(null)
@@ -85,7 +92,7 @@ function cbSupply(obj, data) {
 	for (const build in obj) {
 		result[build] = {
 			...result[build],
-			supply: { ...result?.[build]?.['supply'], ...obj[build] },
+			supply: { ...result?.[build]?.["supply"], ...obj[build] },
 		}
 	}
 	return result
@@ -128,70 +135,88 @@ function check(obj, build, result) {
  * @returns
  */
 function cbAcc(obj, data) {
-	const a = { a: 1, b: 2 }
-	console.log(111, obj, data)
-
-	for (const key in data) {
-		data[key] ??= {}
-		const ksBld = Object.keys(obj[key] ?? {})
-		const keysBld = Object.keys(data[key])
-
-		// Проход добавление-удаление по ключам bld
-		fromFile(keysBld, ksBld, data, obj)
-		// Добавление новых складов в файл
-		fromIter(keysBld, ksBld, data, obj)
+	// Проход по ключам аккумулятора (extralrm, extra, timer ...)
+	for (const key in obj) {
+		// запись пересечений obj c data
+		all(obj[key], data[key])
 	}
-	return data
+	return obj
 }
 
-function fromFile(keysBld, ksBld, data, obj) {
-	keysBld.forEach((bld) => {
-		// Ключ склада не найден в итерации (новых аварий не обнаружено) - удаляем все аварии склада из файла
-		if (!ksBld.includes(bld)) {
-			delete data.extralrm[bld]
-			return
+/**
+ * obj - актуальные аварии, data - сохраненые аварии
+ * Поиск пересечения между двумя объектами, результат мутированный obj,
+ * в котором пересечения копируются из data
+ * @param {*} data данные из файла (слудующая вложенность)
+ * @param {*} obj данные из итерации (следующая вложенность)
+ * @param {*} prev данные из итерации (предыдущая вложенность)
+ * @param {*} key предыдущий ключ
+ * @returns
+ */
+function all(obj, data, prev, key) {
+	const keys = Object.keys(obj)
+	// console.log(111, key, obj, keys)
+	for (const k of keys) {
+		// Ключ есть в файле (пересечение obj c data)
+		if (k in data) {
+			if (typeof obj[k] !== "object") {
+				// console.log(555, key, prev, data)
+				prev[key] = data
+				return
+			}
+			// console.log(444, k, obj)
+			all(obj[k], data[k], obj, k)
 		}
-		// Ключ склада найден в итерации -> добавление/удаление/без_изменений аварий склада
-		const ksAlr = Object.keys(obj.extralrm[bld])
-		const keysAlr = Object.keys(data.extralrm[bld])
-
-		// По авариям склада из файла (удаление,без_изменений)
-		keysAlr.forEach((el) => (!ksAlr.includes(el) ? delete data.extralrm[bld][el] : null))
-		// Добавление новых аварий из итерации
-		ksAlr.forEach((el) => (!keysAlr.includes(el) ? (data.extralrm[bld][el] = obj.extralrm[bld][el]) : null))
-	})
+	}
 }
+// function fromFile(keysBld, ksBld, data, obj) {
+// 	keysBld.forEach((bld) => {
+// 		// Ключ склада не найден в итерации (новых аварий не обнаружено) - удаляем все аварии склада из файла
+// 		if (!ksBld.includes(bld)) {
+// 			delete data.extralrm[bld]
+// 			return
+// 		}
+// 		// Ключ склада найден в итерации -> добавление/удаление/без_изменений аварий склада
+// 		const ksAlr = Object.keys(obj.extralrm[bld])
+// 		const keysAlr = Object.keys(data.extralrm[bld])
 
-function fromIter(keysBld, ksBld, data, obj) {
-	ksBld.forEach((bld) => {
-		// Склад уже записан в файл - пропускаем
-		if (keysBld.includes(bld)) return
-		// Добавляем склад в файл
-		data.extralrm[bld] = obj.extralrm[bld]
-	})
-}
+// 		// По авариям склада из файла (удаление,без_изменений)
+// 		keysAlr.forEach((el) => (!ksAlr.includes(el) ? delete data.extralrm[bld][el] : null))
+// 		// Добавление новых аварий из итерации
+// 		ksAlr.forEach((el) => (!keysAlr.includes(el) ? (data.extralrm[bld][el] = obj.extralrm[bld][el]) : null))
+// 	})
+// }
 
-function all(data, obj) {
-	const adata = Object.keys(data[key])
-	const aobj = Object.keys(obj[key])
-	const r = new Set([...Object.keys(data[key]), ...Object.keys(data[key])])
-	keys.forEach((key) => {
-		// Ключа нет в объекте итерации - удалить из файла
-		if (!(key in obj)) {
-			delete data[key]
-			return
-		}
-		// Ключ есть в объекте итерации
-		// Ключа нет в файле - добавить в файл
-		if (!(key in data)) {
-			data ??= {}
-			data[key] = obj[key]
-			return
-		}
-		// Ключ есть в файле
-		// all()
-	})
-}
+// function fromIter(keysBld, ksBld, data, obj) {
+// 	ksBld.forEach((bld) => {
+// 		// Склад уже записан в файл - пропускаем
+// 		if (keysBld.includes(bld)) return
+// 		// Добавляем склад в файл
+// 		data.extralrm[bld] = obj.extralrm[bld]
+// 	})
+// }
+
+// function all(data, obj) {
+// 	const adata = Object.keys(data[key])
+// 	const aobj = Object.keys(obj[key])
+// 	const r = new Set([...Object.keys(data[key]), ...Object.keys(data[key])])
+// 	keys.forEach((key) => {
+// 		// Ключа нет в объекте итерации - удалить из файла
+// 		if (!(key in obj)) {
+// 			delete data[key]
+// 			return
+// 		}
+// 		// Ключ есть в объекте итерации
+// 		// Ключа нет в файле - добавить в файл
+// 		if (!(key in data)) {
+// 			data ??= {}
+// 			data[key] = obj[key]
+// 			return
+// 		}
+// 		// Ключ есть в файле
+// 		// all()
+// 	})
+// }
 
 // function intersection(data,obj){
 // 	data
@@ -203,7 +228,7 @@ function all(data, obj) {
 // 		if (typeof o[k] !== 'object') return
 // 		getKeys(o)
 // 	})
-// 	return keys 
+// 	return keys
 // }
 
 module.exports = { positionVlv, cbPos, cbTune, cbSupply, cbSmoking, cbAcc }
