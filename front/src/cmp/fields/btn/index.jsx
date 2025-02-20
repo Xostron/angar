14531e6dab22b0f182ae2db660da1b1d6eac2a5c
Xostron <aat9044107480@gmail.com>
2025-02-20 17:26:54 +0300

    fix

--- front/src/cmp/fields/btn/index.jsx
+++ front/src/cmp/fields/btn/index.jsx
@@ -1,13 +1,11 @@
-
-
 //Кнопка с иконкой
-export default function Btn({title, icon, onClick, cls, style}) {
+export default function Btn({ title, icon, onClick, cls, style }) {
 	let cl = ['btn', cls]
 	cl = cl.join(' ')
-	
+
 	return (
 		<button onClick={onClick} className={cl} style={style}>
-			{icon &&<img src={icon}/>}
+			{icon && <img src={icon} />}
 			{title && <span>{title}</span>}
 		</button>
 	)
