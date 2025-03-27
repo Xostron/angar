64452501916cb58338c6fa9f3547f4beb0647514
Xostron <aat9044107480@gmail.com>
2025-03-27 10:05:17 +0300

    fix

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,20 +1,18 @@
 const path = require('path')
-var express = require('express');
-var router = express.Router();
-
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	const r = {
-		code:'Angar API',
+	res.render('index', {
+		code: 'Angar API',
 		config: path.join(process.env.PATH_DATA),
 		retain: path.join(process.env.PATH_RETAIN),
 		factory: path.join(process.env.PATH_FACTORY),
-		uri:path.join(process.env.API_URI),
-		ip:path.join(process.env.IP),
-		version:path.join(process.env.VERSION)		
-	}
-	res.render('index', r);
-});
+		uri: path.join(process.env.API_URI),
+		ip: path.join(process.env.IP),
+		version: path.join(process.env.VERSION),
+	})
+})
 
-module.exports = router;
+module.exports = router
