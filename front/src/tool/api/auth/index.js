c3b0d61cc18c8923b038cbe5bfdc9ebd4eb0c371
Xostron <aat9044107480@gmail.com>
2025-08-07 15:49:17 +0300

    fix

--- front/src/tool/api/auth/index.js
+++ front/src/tool/api/auth/index.js
@@ -1,21 +1,23 @@
 import api from '../config'
 
-async function authLogin(data) {
-	const config = {
-		method: 'POST',
-		url: `web/auth/login`,
-		maxBodyLength: Infinity,
-		data,
-	}
-	return api(config)
-		.then((r) => {
-			console.log(`Response auth/login`, r.data)
-			return r.data
-		})
-		.catch((error) => {
-			console.log(`Error service_angar/${code}`, error)
-			return { error }
-		})
+function authLogin(data) {
+	return new Promise((resolve, reject) => {
+		const config = {
+			method: 'POST',
+			url: `web/auth/login`,
+			maxBodyLength: Infinity,
+			data,
+		}
+		return api(config)
+			.then((r) => {
+				console.log(`Response auth/login`, r.data)
+				resolve(r.data)
+			})
+			.catch((error) => {
+				console.log(`Error service_angar/${code}`, error)
+				reject(error)
+			})
+	})
 }
 
 export { authLogin }
