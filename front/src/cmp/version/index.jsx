5eed806dfe2d86c7e2ee59edcfb3f7b8025ea706
Xostron <aat9044107480@gmail.com>
2025-08-18 17:31:00 +0300

    fix

--- front/src/cmp/version/index.jsx
+++ front/src/cmp/version/index.jsx
@@ -2,6 +2,7 @@ import Helmet from 'react-helmet'
 import useEquipStore from '@store/equipment'
 import { useEffect, useState } from 'react'
 import { useShallow } from 'zustand/react/shallow'
+
 function Version() {
 	const [title, setTitle] = useState('')
 	const list = useEquipStore(useShallow(({ list }) => list))
@@ -11,6 +12,22 @@ function Version() {
 	useEffect(() => {
 		if (name && code) setTitle(`${code} ${name} `)
 	}, [name, code])
+
+	useEffect(() => {
+		console.log('555')
+		get('net_info', api)
+			.then((o) => {
+				setInfo(o.net)
+				setTtyS(o.ttyS)
+				notification.success('Информация о сети обновлена')
+			})
+			.catch((e) => {
+				setReqIp('127.0.0.1')
+				notification.error(e.message || e.error || 'Ошибка получения информации о сети', {
+					errorId: e.id,
+				})
+			})
+	}, [])
 
 	return (
 		<div style={{ position: 'absolute', bottom: '15px', right: '15px', color: 'darkgray' }}>
