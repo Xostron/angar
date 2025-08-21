593c78098d0fee4c334be0f2fb84dd3b7deaec45
Xostron <aat9044107480@gmail.com>
2025-08-21 17:01:40 +0300

    fix

--- front/src/cmp/warn/index.jsx
+++ front/src/cmp/warn/index.jsx
@@ -1,4 +1,4 @@
-import useWarn from '@store/warn'
+import useWarnStore from '@store/warn'
 import Dialog from '@cmp/dialog'
 import useDialog from '@cmp/dialog/hook'
 import def from './def'
@@ -6,7 +6,7 @@ import def from './def'
 // Предупреждение и информирование
 export default function Warn({}) {
 	const { refDialog, open, close } = useDialog()
-	const { clear, data, show, entryCode } = useWarn(({ clear, data, show, entryCode }) => ({
+	const { clear, data, show, entryCode } = useWarnStore(({ clear, data, show, entryCode }) => ({
 		clear,
 		data,
 		show,
