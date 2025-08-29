6d1f171f623dea86edf8e339404356d91b0c3711
Xostron <aat9044107480@gmail.com>
2025-08-29 10:48:08 +0300

    fix

--- front/src/page/service/def/index.js
+++ front/src/page/service/def/index.js
@@ -2,13 +2,13 @@ import Equip from './equip'
 import Network from './network'
 import Soft from './soft'
 import Pos from './pos'
-import Journal from './journal'
+import journal from './journal'
 const def = {
 	1: Network,
 	2: Equip,
 	3: Soft,
 	4: Pos,
-	journal: Journal,
+	journal
 }
 
 export default def
