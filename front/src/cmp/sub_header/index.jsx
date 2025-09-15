a759ea1cca7fc4ca1b791a62919cb2d05ec4edb3
Xostron <aat9044107480@gmail.com>
2025-09-15 17:37:26 +0300

    fix

--- front/src/cmp/sub_header/index.jsx
+++ front/src/cmp/sub_header/index.jsx
@@ -2,17 +2,37 @@ import Message from '@cmp/message'
 import Person from '@cmp/person'
 import Prod from '@cmp/prod'
 import Turn from '@cmp/turn'
-
+import useViewStore from '@store/view'
+let stl = {
+	gridArea: '1 / 2 / 2 / 3',
+	display: 'flex',
+	justifyContent: 'space-between',
+}
 //Позаголовок в странице склада
 export default function SubHeader({}) {
-	return (
+	const mb = useViewStore((s) => s.mb())
+
+	return !mb ? (
+		// desktop
 		<>
 			<Turn style={{ gridArea: '1 / 1 / 1 / 1' }} />
-			<div style={{ gridArea: '1 / 2 / 2 / 3', display: 'flex', justifyContent: 'space-between' }}>
+			<Person style={{ gridArea: '1 / 3 / 1 / 3', justifySelf: 'end' }} />
+			<div style={stl}>
 				<Prod />
 				<Message />
 			</div>
-			<Person style={{ gridArea: '1 / 3 / 1 / 3', justifySelf:'end' }} />
 		</>
+	) : (
+		// mobile
+		<section className={'cmp-subheader-mb'} style={{ gridArea: '1 / 1 / 3 / 1' }}>
+			<div>
+				<Turn />
+				<Person />
+			</div>
+			<div>
+				<Prod />
+				<Message />
+			</div>
+		</section>
 	)
 }
--- front/src/page/build_or_sect/index.jsx
+++ front/src/page/build_or_sect/index.jsx
@@ -3,14 +3,15 @@ import { useParams } from 'react-router-dom'
 import SubHeader from '@cmp/sub_header'
 import Outdoor from '@cmp/outdoor'
 import Banner from '@cmp/banner'
+import useViewStore from '@store/view'
 
 //Секции склада / Секция
 export default function BuildOrSect({}) {
 	let { sect } = useParams()
 	const type = sect ? 'section' : 'building'
-
+	const mb = useViewStore((s) => s.mb())
 	return (
-		<main className='build'>
+		<main className={`build ${mb}`}>
 			<SubHeader />
 			<Outdoor />
 			<Outlet />
