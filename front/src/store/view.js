5260e9a913fd382b1beac839e8ebfc82f6230d0e
Xostron <aat9044107480@gmail.com>
2025-09-19 11:36:18 +0300

    fix

--- front/src/store/view.js
+++ front/src/store/view.js
@@ -1,13 +1,14 @@
 import { create } from 'zustand'
 
-const EDGE = 1024
+const TABLET = 1024
+const MOBILE = 768
 
 // Размер экрана
 const useViewStore = create((set, get) => ({
-	view: window.innerWidth < EDGE ? 'mobile' : 'pc',
+	view: window.innerWidth < MOBILE ? 'mobile' : 'pc',
 	updateView: (_) => {
 		let view = window.innerWidth < window.screen.width ? window.innerWidth : window.screen.width
-		 view = view < EDGE ? 'mobile' : 'pc'
+		 view = view < MOBILE ? 'mobile' : 'pc'
 		if (get().view === view) return
 		set({ view })
 	},
