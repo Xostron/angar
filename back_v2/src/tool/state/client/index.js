783d6c3528a2120c821362aca7cff2dc0448ce87
Xostron <aat9044107480@gmail.com>
2025-10-08 11:07:07 +0300

    fix

--- back_v2/src/tool/state/client/index.js
+++ back_v2/src/tool/state/client/index.js
@@ -65,7 +65,7 @@ module.exports = async function state() {
 		o.hub.last = new Date()
 		o.hub.state = o.diffing === null ? o.present : { ...o.hub.state, ...o.diffing }
 		console.log('\x1b[33m%s\x1b[0m', '3. ✅POS->Tenta: Данные переданы', o?.result?.length)
-		console.log(4, JSON.stringify(o.result, null, ' '))
+		console.log(4, o.result)
 		return true
 	} catch (error) {
 		throw error
