636f5581b106791b7bbbe1ebc5dddc5c2c5e093b
Xostron <aat9044107480@gmail.com>
2025-12-09 14:41:48 +0300

    clear alarm auto

--- back/src/tool/message/auto.js
+++ back/src/tool/message/auto.js
@@ -3,30 +3,43 @@ const { data } = require('@store')
 /**
  * Аварии автоматического режима (темп улицы не подходит и т.д.)
  * rs-триггер (приоритет на сброс)
- * @param {*} buildingId
+ * @param {*} idB
  * @param {*} sectionId
  * @param {*} automode
  * @param {*} arr {id код аварии, set условие установки аварии, reset условие сброса аварии, msg текст аварии}
  */
-function rs(buildingId, automode, arr) {
+function rs(idB, automode, arr) {
 	data.alarm.auto ??= {}
-	data.alarm.auto[buildingId] ??= {}
-	data.alarm.auto[buildingId][automode] ??= {}
+	data.alarm.auto[idB] ??= {}
+	data.alarm.auto[idB][automode] ??= {}
 	if (!arr?.length) return
-
-	const d = data?.alarm?.auto?.[buildingId]?.[automode]
+	const d = data.alarm.auto[idB][automode]
 
 	arr.forEach((o, idx) => {
-		let r = null
 		if (o.set && !d?.[o.msg.code]) d[o.msg.code] = { id: idx, ...o.msg }
 		if (o.reset) delete d[o.msg.code]
 	})
 }
 
-// Наличие аварии авторежима
-function isAlr(buildingId, automode) {
-	const d = data.alarm.auto?.[buildingId]?.[automode] ?? {}
+
+/**
+ * Наличие аварии авторежима
+ * @param {*} idB ИД склада
+ * @param {*} automode Авторежим
+ * @returns {boolean} true - взведена авария авторежима
+ */
+function isAlr(idB, automode) {
+	const d = data.alarm.auto?.[idB]?.[automode] ?? {}
 	return Object.keys(d).length ? true : false
 }
+/**
+ * Очистка аккумулятора с авариями
+ * @param {*} idB ИД склада
+ * @param {*} automode Авторежим
+ */
+function clearAA(idB, automode) {
+	data.alarm.auto[idB] ??= {}
+	data.alarm.auto[idB][automode] = {}
+}
 
-module.exports = { rs, isAlr }
+module.exports = { rs, isAlr, clearAA }
