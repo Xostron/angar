dd328cfc813824cb229daf3f44fb98b7be1f06b9
Xostron <aat9044107480@gmail.com>
2026-01-15 17:30:02 +0300

    fix

--- front/src/page/main/list/item/index.jsx
+++ front/src/page/main/list/item/index.jsx
@@ -7,28 +7,28 @@ import useViewStore from '@store/view'
 import useInputStore from '@store/input'
 import './style.css'
 
-
-export default function Item({ item, idx, buildId}) {
+export default function Item({ item, idx, buildId }) {
 	const [start] = useInputStore(useShallow(({ input }) => [input?.retain?.[buildId]?.start]))
 	const mb = useViewStore((s) => s.mb())
 	const { _id } = item
 	let cl = ['item']
-	if(!item?.on) cl.push('out')
+	if (!item?.on) cl.push('out')
 	if (mb) cl.push(mb)
 	cl = cl.join(' ')
-    
 
 	return (
 		<Link className={cl} to={`/building/${item._id}`}>
 			<div>
 				<div className='top'>
-					<p>{item?.code}</p>
-					<span className={start?'on':'off'}>{start ? 'Вкл.' : 'Выкл.'}</span>
+					<p>
+						{item?.code} {item?.name}
+					</p>
+					<span className={start ? 'on' : 'off'}>{start ? 'Вкл.' : 'Выкл.'}</span>
 				</div>
-				<Mode buildingId={_id} type = {item?.type}/>
+				<Mode buildingId={_id} type={item?.type} />
 				<div className='bottom'>
 					<Tout buildingId={_id} />
-					<Alarm buildId={buildId}/>
+					<Alarm buildId={buildId} />
 				</div>
 			</div>
 		</Link>
