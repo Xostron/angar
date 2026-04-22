95223d0ee39d7595f1b31b308a1ea456230b61e1
Xostron <aat9044107480@gmail.com>
2026-04-22 14:29:43 +0300

    fix 5.8.16

--- back/src/routes/api/tenta/post/index.js
+++ back/src/routes/api/tenta/post/index.js
@@ -36,27 +36,24 @@ async function feedback(obj) {
 	const cur = store.cycleId
 	let repeat = 0
 
-	// Ждем 10 сек (предохранитель) или завершение текщего основного цикла программы
-	while (cur === store.cycleId && repeat < 10) {
+	// Ждем завершение текщего основного цикла программы
+	while (cur === store.cycleId) {
+		// Ждем 1 сек
 		const done = await delay(1000)
 		repeat += done ? 1 : 0
-		console.log(1, 'Ждем конца цикла', cur, store.cycleId, cur === store.cycleId, repeat)
-	}
-
-	// Дождались изменения:
-	// по циклу программы -> возврат state и retain
-	if (cur !== store.cycleId) {
-		console.log(2, 'Подготовка ответа')
-		// state ангара (карточки складов и т.д.)
-		const { result = [], present = [] } = await prepare('init')
-		// retain
-		return {
-			state: result,
-			retain: obj?.buildingId ? store.retain[obj?.buildingId] : store.retain,
+		// Предохранитель: если за 10 сек цикл программы не изменился, то выход
+		if (repeat > 10) {
+			console.log(2, 'Выход по таймауту')
+			return null
 		}
+		// console.log(1, 'Ждем конца цикла', cur, store.cycleId, cur === store.cycleId, repeat)
 	}
 
-	// По предохранителю -> Ошибка сервера ангара
-	console.log(2, 'Выход по таймауту')
-	return null
+	// console.log(2, 'Подготовка ответа')
+	// Дождались изменения: state ангара (карточки складов и т.д.), retain склада
+	const { result = [], present = [] } = await prepare('init')
+	return {
+		state: result,
+		retain: obj?.buildingId ? store.retain[obj?.buildingId] : store.retain,
+	}
 }
