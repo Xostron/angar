f8fc74d95bd8ccd588b3e5bab594ad8cdf28408d
Xostron <aat9044107480@gmail.com>
2026-05-06 14:54:56 +0300

    fix для виктора

--- back/src/routes/api/tenta/read/store/transform/sp/index.js
+++ back/src/routes/api/tenta/read/store/transform/sp/index.js
@@ -7,16 +7,33 @@ function sp(bldId, type, automode) {
 
 	if (type === 'cold') {
 		const r = { tprd: acc.target }
-		// console.log(9999, r)
+		console.log(9999, r)
+		return r
+	}
+
+	if (type === 'combi') {
+		const r = {
+			tprd:
+				isNaN(acc?.cold?.tgtTprd) || acc?.cold?.tgtTprd === null
+					? acc?.cold?.tgtTprd
+					: +acc?.cold?.tgtTprd?.toFixed(1),
+			tcnl:
+				isNaN(acc?.cold?.tgtTcnl) || acc?.cold?.tgtTcnl === null
+					? acc?.cold?.tgtTcnl
+					: +acc?.cold?.tgtTcnl?.toFixed(1),
+			p: store.calcSetting?.[bldId]?.fan?.pressure?.p,
+			hin: acc.setting?.mois?.humidity,
+		}
+		console.log(999, r)
 		return r
 	}
 	const r = {
-		tprd: acc.tgt,
-		tcnl: acc.tcnl,
+		tprd: isNaN(acc.tgt) ? acc.tgt : +acc.tgt.toFixed(1),
+		tcnl: isNaN(acc.tcnl) ? acc.tcnl : +acc.tcnl.toFixed(1),
 		p: store.calcSetting?.[bldId]?.fan?.pressure?.p,
 		hin: acc.setting?.mois?.humidity,
 	}
-	// console.log(999, r)
+	console.log(99, r)
 	return r
 }
 
