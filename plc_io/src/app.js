a477c9d2960165ff0bc9293489267e94d33561b0
Xostron <aat9044107480@gmail.com>
2026-05-22 17:28:22 +0300

    fix

--- plc_io/src/app.js
+++ plc_io/src/app.js
@@ -3,8 +3,28 @@ const engineRouters = require('./routes')
 
 // Инициализация fastify, подключение логирования
 const app = fastify({
-	logger:
-		process.env.NODE_ENV === 'development' ? { transport: { target: 'pino-pretty' } } : true,
+	logger: {
+		transport: {
+			target: 'pino-pretty', // если вы используете красивый вывод в консоль
+			options: {
+				translateTime: 'HH:MM:ss.l', // формат времени как у вас [16:25:44.169]
+				ignore: 'pid,hostname,req', // скрываем стандартный большой объект req и лишние поля
+			},
+		},
+		// Переносим метод и URL в саму строку сообщения INFO
+		formatters: {
+			log(object) {
+				if (object.req) {
+					object.msg = `incoming request: ${object.req.method} "${object.req.url}"`
+				}
+				return object
+			},
+		},
+		// Оставляем сериализатор пустым, чтобы объект req вообще не писался в лог
+		serializers: {
+			req: () => undefined,
+		},
+	},
 })
 
 // Роуты
