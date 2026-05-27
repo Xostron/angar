9eb8e5fd9da3479f4d68532969ed79fbbe863bc2
Xostron <aat9044107480@gmail.com>
2026-05-27 17:16:56 +0300

    fix merge

--- back/src/routes/api/plc_io/get/index.js
+++ back/src/routes/api/plc_io/get/index.js
@@ -1,8 +1,11 @@
+const { live } = require('@store/index')
+
 const readJson = require('@tool/json').read
 
 // Ответ микросервису rw: рама модулей и оборудования
 function get() {
 	return function (req, res) {
+		live()
 		readJson(['module', 'equipment']).then(([module, equipment]) => {
 			res.status(200).json({ module, equipment })
 		})
