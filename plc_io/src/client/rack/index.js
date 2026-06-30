6ddfbea77ba7667a80acbf342943097fced6eb0c
Xostron <aat9044107480@gmail.com>
2026-06-30 17:01:07 +0300

    fix docs

--- plc_io/src/client/rack/index.js
+++ plc_io/src/client/rack/index.js
@@ -1,43 +1,43 @@
-const api = require('@tool/api')
-const { delay } = require('@tool/time')
-const { store } = require('@store/index')
+// const api = require('@tool/api')
+// const { delay } = require('@tool/time')
+// const { store } = require('@store/index')
 
-/**
- * Запрос рамы модулей и оборудования
- * Обновление каждые 7 мин по-умолчанию | Если нет рамы, то каждые 10 сек
- */
-async function loopRack() {
-	while (true) {
-		// Запрос рамы module, аварийных сообщений alarm
-		await getRack()
+// /**
+//  * Запрос рамы модулей и оборудования
+//  * Обновление каждые 7 мин по-умолчанию | Если нет рамы, то каждые 10 сек
+//  */
+// async function loopRack() {
+// 	while (true) {
+// 		// Запрос рамы module, аварийных сообщений alarm
+// 		await getRack()
 
-		// [Обновление каждые 7 мин по-умолчанию] : [Если нет рамы, то каждые 10 сек]
-		store._handshake ? await delay(store._period ?? 420001) : await delay(10000)
-	}
-}
+// 		// [Обновление каждые 7 мин по-умолчанию] : [Если нет рамы, то каждые 10 сек]
+// 		store._handshake ? await delay(store._period ?? 420001) : await delay(10000)
+// 	}
+// }
 
-async function getRack() {
-	const config = {
-		method: 'GET',
-		url: 'io/init',
-		headers: {},
-	}
+// async function getRack() {
+// 	const config = {
+// 		method: 'GET',
+// 		url: 'io/init',
+// 		headers: {},
+// 	}
 
-	const r = await api(config)
-	// Нет связи с сервером ангара
-	if (!r?.data || !r?.data?.module || !r?.data?.equipment)
-		return console.log('🔴 plc_io -> back. Невозможно получить раму - нет связи с ангаром')
+// 	const r = await api(config)
+// 	// Нет связи с сервером ангара
+// 	if (!r?.data || !r?.data?.module || !r?.data?.equipment)
+// 		return console.log('🔴 plc_io -> back. Невозможно получить раму - нет связи с ангаром')
 
-	// Есть связь
-	store.live()
+// 	// Есть связь
+// 	store.live()
 
-	store.module = r.data.module
-	store.equipment = r.data.equipment
-	store.alarm.module = r.data.alarm ?? {}
+// 	store.module = r.data.module
+// 	store.equipment = r.data.equipment
+// 	store.alarm.module = r.data.alarm ?? {}
 
-	store._handshake = true
-	console.log('🟢 Рама от ангара получена')
-	return true
-}
+// 	store._handshake = true
+// 	console.log('🟢 Рама от ангара получена')
+// 	return true
+// }
 
-module.exports = { loopRack }
+// module.exports = { loopRack }
--- plc_io/src/client/value/index.js
+++ plc_io/src/client/value/index.js
@@ -1,46 +1,46 @@
-const api = require('@tool/api')
-const { store } = require('@store/index')
+// const api = require('@tool/api')
+// const { store } = require('@store/index')
 
-const apiConfig = (data, params) => ({
-	method: 'POST',
-	maxBodyLength: Infinity,
-	url: 'io/value',
-	headers: {
-		'Content-Type': 'application/json',
-		// ip: process.env.IP,
-		// 'X-Client-Timezone': timezone,
-		// 'X-Client-Timezone-Offset': offset,
-	},
-	data,
-	params,
-})
+// const apiConfig = (data, params) => ({
+// 	method: 'POST',
+// 	maxBodyLength: Infinity,
+// 	url: 'io/value',
+// 	headers: {
+// 		'Content-Type': 'application/json',
+// 		// ip: process.env.IP,
+// 		// 'X-Client-Timezone': timezone,
+// 		// 'X-Client-Timezone-Offset': offset,
+// 	},
+// 	data,
+// 	params,
+// })
 
-/**
- * Отправка данных опроса модулей на сервер ангара
- * @returns
- */
-async function postV() {
-	try {
-		// Данные не готовы
-		if (!Object.keys(store.v ?? {}).length)
-			return console.log('🟡 plc_io -> back. Невозможно опросить модули - нет рамы')
+// /**
+//  * Отправка данных опроса модулей на сервер ангара
+//  * @returns
+//  */
+// async function postV() {
+// 	try {
+// 		// Данные не готовы
+// 		if (!Object.keys(store.v ?? {}).length)
+// 			return console.log('🟡 plc_io -> back. Невозможно опросить модули - нет рамы')
 
-		// Запрос
-		const config = apiConfig({ v: store.v, alarm: store.alarm.module }, {})
-		const r = await api(config)
+// 		// Запрос
+// 		const config = apiConfig({ v: store.v, alarm: store.alarm.module }, {})
+// 		const r = await api(config)
 
-		// Запрос не успешен
-		if (!r.data) {
-			throw new Error('🔴 plc_io -> back. Опрос модулей не передан - нет связи с ангаром')
-		}
+// 		// Запрос не успешен
+// 		if (!r.data) {
+// 			throw new Error('🔴 plc_io -> back. Опрос модулей не передан - нет связи с ангаром')
+// 		}
 
-		// Запрос успешен
-		console.log('🟢 plc_io -> back. Опрос модулей успешно передан на ангар')
-		store.live()
-		return true
-	} catch (error) {
-		console.error(error)
-	}
-}
+// 		// Запрос успешен
+// 		console.log('🟢 plc_io -> back. Опрос модулей успешно передан на ангар')
+// 		store.live()
+// 		return true
+// 	} catch (error) {
+// 		console.error(error)
+// 	}
+// }
 
-module.exports = postV
+// module.exports = postV
