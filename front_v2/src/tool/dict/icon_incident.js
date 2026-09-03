19ed07d3c4f4aa46e796dabb212a0262e7756ba1
Xostron <aat9044107480@gmail.com>
2026-09-03 11:37:00 +0300

    fix

--- front_v2/src/tool/dict/icon_incident.js
+++ front_v2/src/tool/dict/icon_incident.js
@@ -2,6 +2,67 @@ const iconIncident = {
   undefined: '/icon/indicator/info.svg',
   null: '/icon/indicator/info.svg',
   '': '/icon/indicator/info.svg',
+  // notification Уведомление
+  //   Сушка
+  drying_tout1: '/icon/incident/notification/tout1-1.svg',
+  drying_tout2: '/icon/incident/notification/tout2-1.svg',
+  drying_hout1: '/icon/incident/notification/hout1-2.svg',
+  drying_hout2: '/icon/incident/notification/hout1-1.svg',
+  drying_ahout1: '/icon/incident/notification/ahout1-1.svg',
+  drying_ahout2: '/icon/incident/notification/ahout2-1.svg',
+  // Хранение
+  cooling_tout1: '/icon/incident/notification/tout1.svg',
+  cooling_tout2: '/icon/incident/notification/tout2.svg',
+  cooling_tout3: '/icon/incident/notification/tout3.svg',
+  cooling_hout1: '/icon/incident/notification/hout1.svg',
+  cooling_hout2: '/icon/incident/notification/hout2.svg',
+  cooling_hout3: '/icon/incident/notification/hout3.svg',
+  cooling_ahout1: '/icon/incident/notification/ahout1.svg',
+  cooling_ahout2: '/icon/incident/notification/ahout2.svg',
+  // Дефростация
+  defrost_tout1: '/icon/incident/notification/tout1.svg',
+  defrost_tout2: '/icon/incident/notification/tout2.svg',
+  defrost_tout3: '/icon/incident/notification/tout3.svg',
+  defrost_hout1: '/icon/incident/notification/hout1.svg',
+  defrost_hout2: '/icon/incident/notification/hout2.svg',
+  defrost_hout3: '/icon/incident/notification/hout3.svg',
+  defrost_ahout1: '/icon/incident/notification/ahout1.svg',
+  defrost_ahout2: '/icon/incident/notification/ahout2.svg',
+  //   Таймеры запретов
+  drying: '/icon/incident/notification/drying.svg',
+  cure: '/icon/incident/notification/cure.svg',
+  cooling: '/icon/incident/notification/cooling.svg',
+  heat: '/icon/incident/notification/heat.svg',
+  vent: '/icon/incident/notification/vent.svg',
+  accel: '/icon/incident/notification/accel.svg',
+  ozon: '/icon/incident/notification/ozon.svg',
+  heater: '/icon/incident/notification/heater.svg',
+  co2: '/icon/incident/notification/co2.svg',
+  wetting: '/icon/incident/notification/wetting.svg',
+
+  // warning Предупреждление
+  antibliz: '/icon/incident/warning/antibliz.svg',
+  debdo: '/icon/incident/warning/debdo.svg',
+  openVin: '/icon/incident/warning/openVin.svg',
+  overVlv: '/icon/incident/warning/overVlv.svg',
+  stableVno: '/icon/incident/warning/stableVno.svg',
+
+  //   equipment Работа оборудования
+  co2wait: '/icon/incident/equipment/co2wait.svg',
+  co2work: '/icon/incident/equipment/co2work.svg',
+  smoking1: '/icon/incident/equipment/smoking1.svg',
+  smoking2: '/icon/incident/equipment/smoking2.svg',
+  ventDura: '/icon/incident/equipment/ventDura.svg',
+  ventOn: '/icon/incident/equipment/ventOn.svg',
+  ventTime: '/icon/incident/equipment/ventTime.svg',
+  ventTimeWait: '/icon/incident/equipment/ventTimeWait.svg',
+
+  //   alarm Авария
+  alrClosed: '/icon/incident/alarm/alrClosed.svg',
+  battery: '/icon/incident/alarm/battery.svg',
+  connect: '/icon/incident/alarm/connect.svg',
+  local: '/icon/incident/alarm/local.svg',
+  notTune: '/icon/incident/alarm/notTune.svg',
 };
 
 export default iconIncident;
