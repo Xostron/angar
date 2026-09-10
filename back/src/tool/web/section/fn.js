2a54819625928a8d747b054b28f3483b45e8682e
Xostron <aat9044107480@gmail.com>
2026-09-10 17:17:48 +0300

    fix

--- back/src/tool/web/section/fn.js
+++ back/src/tool/web/section/fn.js
@@ -76,7 +76,6 @@ function heatVlv(idS, obj) {
  */
 function fnSBarB() {
 	const r = {}
-	console.log(11, store.value?.alarm?.monit)
 	for (const idB in store.value?.alarm?.barB) {
 		r[idB] = []
 		for (const code in store.value?.alarm?.barB[idB]) {
