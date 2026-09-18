4be15880c995515d49925b3e5a73da6f93fe0cdb
Xostron <aat9044107480@gmail.com>
2026-09-18 17:09:05 +0300

    fix

--- front/src/page/main/list/item/index.jsx
+++ front/src/page/main/list/item/index.jsx
@@ -8,22 +8,28 @@ import useInputStore from '@store/input'
 import useRemoteStore from '@store/remote'
 // import useDialog from '@cmp/dialog/hook'
 import './style.css'
-import Remote from './remote';
+import Remote from './remote'
 
 export default function Item({ item }) {
-	const remoteBCard = useRemoteStore(useShallow((s) => {
-		if (!item.remote) return null
-		const b = s.buildings?.[item.bldId]
-		if (!b) return null
-		return { ...b, device: s.devices?.[b.deviceId] }
-	}))
-	const inputBCard = useStoreWithEqualityFn(useInputStore, (s) => item.remote ? null : s.input?.bCard?.[item._id] ?? null, (a, b) => JSON.stringify(a) === JSON.stringify(b))
+	const remoteBCard = useRemoteStore(
+		useShallow((s) => {
+			if (!item.remote) return null
+			const b = s.buildings?.[item.bldId]
+			if (!b) return null
+			return { ...b, device: s.devices?.[b.deviceId] }
+		}),
+	)
+	const inputBCard = useStoreWithEqualityFn(
+		useInputStore,
+		(s) => (item.remote ? null : (s.input?.bCard?.[item._id] ?? null)),
+		(a, b) => JSON.stringify(a) === JSON.stringify(b),
+	)
 	const doc = item.remote ? remoteBCard : inputBCard
 	const mb = useViewStore((s) => s.mb())
 	let cl = ['item']
 	if (mb) cl.push(mb)
 	cl = cl.join(' ')
-
+	console.log(11, item.remote, remoteBCard, inputBCard)
 	if (item.remote) return <Remote cl={cl} doc={doc} item={item} />
 	return (
 		<Link className={cl} to={`/building/${item._id}`}>
@@ -32,12 +38,14 @@ export default function Item({ item }) {
 					<p>
 						{doc?.code} {doc?.name}
 					</p>
-						<span className={doc?.sidesect?.start ? 'on' : 'off'}>{doc?.sidesect?.start ? 'Вкл.' : 'Выкл.'}</span>
+					<span className={doc?.sidesect?.start ? 'on' : 'off'}>
+						{doc?.sidesect?.start ? 'Вкл.' : 'Выкл.'}
+					</span>
 				</div>
 				<Mode doc={doc} />
-					<div className='bottom'>
-						<Tout doc={doc} />
-					</div>
+				<div className='bottom'>
+					<Tout doc={doc} />
+				</div>
 			</div>
 		</Link>
 	)
--- front/src/page/main/list/item/tout/index.jsx
+++ front/src/page/main/list/item/tout/index.jsx
@@ -1,8 +1,8 @@
 import './style.css'
 
 export default function Tout({ doc }) {
-	if(!doc) return null
-	const {tprd, hin, countAlr} = doc 
+	if (!doc) return null
+	const [tmin, tmax, hin] = doc.sensor
 	const imgT = '/img/periphery/temp/on.svg'
 	const imgWet = '/img/periphery/moisture/on.svg'
 	const imgAlrm = '/img/alarm/alr.svg'
@@ -12,24 +12,24 @@ export default function Tout({ doc }) {
 			{/* Влажность */}
 			<div className={`temp ${hin?.state ?? 'off'}`}>
 				<img src={imgWet} />
-				{hin?.value!=null ? <span> {hin.value} %</span> : null}
+				{hin?.value != null ? <span> {hin.value} %</span> : null}
 			</div>
 			{/* Температура продукта min */}
-			<div className={`temp ${tprd?.state ?? ''}`}>
+			<div className={`temp ${tmin?.state ?? ''}`}>
 				<img src={imgT} />
-				{tprd?.min !=null ? <span> min {tprd?.min} °C</span> : null}
+				{tmin?.value != null ? <span> min {tmin?.value} °C</span> : null}
 			</div>
 			{/* Температура подукта max  */}
-			<div className={`temp ${tprd?.state  ?? ''}`}>
+			<div className={`temp ${tmax?.state ?? ''}`}>
 				<img src={imgT} />
-				{tprd?.max !=null ? <span> max {tprd?.max } °C</span> : null}
+				{tmax?.value != null ? <span> max {tmax?.value} °C</span> : null}
 			</div>
 			{/* Аварии */}
-			{countAlr ? (
+			{doc?.countAlr ? (
 				<div className={`msg alarm`}>
 					<img src={imgAlrm} />
 					<span>Сообщения</span>
-					<span className='count'>{countAlr}</span>
+					<span className='count'>{doc?.countAlr}</span>
 				</div>
 			) : null}
 		</div>
