b7006be71ee4be4876474d57c323b25503453874
Xostron <aat9044107480@gmail.com>
2026-09-24 17:00:17 +0300

    fix Обновление рамы только при старте сервера

--- back/src/client/init/index.js
+++ back/src/client/init/index.js
@@ -1,13 +1,16 @@
 const { delay } = require('@tool/command/time')
 const { init } = require('@tool/init')
 
-// Запрос рамы у админки, каждые 7 мин
+// Запрос рамы у админки (Только при старте, с рандомной задержкой от 0 до 30сек)
 async function loopInit() {
-	while (true) {
-		init()
-		// 7 минут
-		await delay(process.env?.PERIOD ?? 420001)
-	}
+	// while (true) {
+	const randomNumber = Math.floor(Math.random() * 30001)
+	console.log(123, randomNumber)
+	await delay(randomNumber)
+	// await delay(process.env?.PERIOD ?? 420001)
+	init()
+	// 7 минут
+	// }
 }
 
 module.exports = loopInit
